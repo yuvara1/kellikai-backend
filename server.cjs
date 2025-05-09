402499d37f0263b65ad37270cf87ae32f9b5918b
YUVARAJ <yuvarajacb11@gmail.com>
2025-05-09 15:45:35 +0530

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -7,7 +7,7 @@ const fs = require('fs');
 
 
 const app = express();
-const port = process.env.PORT || 10000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -214,7 +214,7 @@ app.get('/getallusers', async (req, res) => {
 // Serve uploaded files
 app.get('/uploads/:filename', (req, res) => {
      const { filename } = req.params;
-     const filePath = filename;
+     const filePath = `http://localhost:3000/uploads/`+filename;
      console.log('File path:', filePath); // Log the file path for debugging
      fs.readFile(filePath, (err, data) => {
           if (err) {
