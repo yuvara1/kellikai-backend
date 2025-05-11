294244a5068b424abf8c7a41886817803b71e4d1
YUVARAJ <yuvarajacb11@gmail.com>
2025-05-11 13:42:28 +0530

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -9,7 +9,7 @@ const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const cors = require('cors');
+
 app.use(cors({ origin: 'http://localhost:5173' })); // Replace with your frontend's URL
 app.use(cors());
 app.use(express.json());
